fb01a7699d083c731c7558413cdda6ac2c871132
Gerson Murguia Montes <aldmurguia1@gmail.com>
2023-09-21 14:48:14 -0500

    Creación de proyecto con Spring Boot 3 usando Java 17

---
+++ src/main/java/avatar/global/examen_backend/ExamenBackendApplication.java
@@ -0,0 +1,14 @@
+package avatar.global.examen_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ExamenBackendApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(ExamenBackendApplication.class, args);
+    }
+
+}
