6422b5ce2f6689be3c618543eda2cf39454fb399
Gerson Murguia Montes <aldmurguia1@gmail.com>
2023-09-21 15:49:10 -0500

    Creado Bean de restTemplate

--- src/main/java/avatar/global/examen_backend/ExamenBackendApplication.java
+++ src/main/java/avatar/global/examen_backend/ExamenBackendApplication.java
@@ -2,6 +2,8 @@ package avatar.global.examen_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
@@ -10,5 +12,8 @@ public class ExamenBackendApplication {
     public static void main(final String[] args) {
         SpringApplication.run(ExamenBackendApplication.class, args);
     }
-
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
